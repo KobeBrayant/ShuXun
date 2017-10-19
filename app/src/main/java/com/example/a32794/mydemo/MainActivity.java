71e7df57f3d547a82e214dbdb5292916cd71a513
32794 <327943014@qq.com>
2017-10-19 09:24:41 +0800

    好使了没

--- app/src/main/java/com/example/a32794/mydemo/MainActivity.java
+++ app/src/main/java/com/example/a32794/mydemo/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.a32794.mydemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
